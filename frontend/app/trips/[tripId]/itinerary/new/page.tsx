0f036e5f44b330359ae24a34ba38d75934aec026
Ngo-Thanh-Quang <quangngo2316@gmail.com>
2025-10-01 00:13:20 +0700

    Update Structure

---
+++ frontend/app/trips/[tripId]/itinerary/new/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <h1>Create a New Itinerary</h1>
+      {/* Form and other components go here */}
+    </div>
+  )
+}
+
+export default page
